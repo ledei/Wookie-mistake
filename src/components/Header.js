e7647b425ec1ed6fa731dd4e4e6a974759802019
ledei <holybuzz@live.se>
2023-02-01 20:23:08 +0100

    made a indicator for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,95 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export function Header() {
+  const [active, setActive] = useState("home");
+
+  function handleClick(activate) {
+    console.log(activate);
+    setActive(activate);
+  }
   return (
     <header>
-      { <nav> 
-  
-        <ul className="list-container">
-          <li className="list">
-            <Link to="/"className="beta-test">Home</Link>
-          </li>
-          <li className="list">
-            <Link to="/Planets" className="beta-test" onClick={}>Planets</Link>
-          </li>
-          <li className="list">
-            <Link to="/People" className="beta-test">People</Link>
-          </li>
-          <li className="list">
-            <Link to="/Species" className="beta-test">Species</Link>
-          </li>
-          <li className="list">
-            <Link to="/Vehicles" className="beta-test">Vehicles</Link>
-          </li>
-          <li className="list">
-            <Link to="/Starships" className="beta-test">Starships</Link>
-          </li>
-          <li className="list">
-            <Link to="/Films" className="beta-test"> Films</Link>
-          </li>
-        </ul> 
-        
-      </nav> }  
-      
+      {
+        <nav>
+          <ul className="list-container">
+            <li
+              className="list"
+              onClick={() => {
+                handleClick("home");
+              }}>
+              <Link to="/" className={active === "home" ? "active" : ""}>
+                Home
+              </Link>
+            </li>
+            <li
+              className="list"
+              onClick={() => {
+                handleClick("planets");
+              }}>
+              <Link
+                to="/Planets"
+                className={active === "planets" ? "active" : ""}>
+                Planets
+              </Link>
+            </li>
+            <li
+              className="list"
+              onClick={() => {
+                handleClick("people");
+              }}>
+              <Link
+                to="/People"
+                className={active === "people" ? "active" : ""}>
+                People
+              </Link>
+            </li>
+            <li
+              className="list"
+              onClick={() => {
+                handleClick("species");
+              }}>
+              <Link
+                to="/Species"
+                className={active === "species" ? "active" : ""}>
+                Species
+              </Link>
+            </li>
+            <li
+              className="list"
+              onClick={() => {
+                handleClick("vehicles");
+              }}>
+              <Link
+                to="/Vehicles"
+                className={active === "vehicles" ? "active" : ""}>
+                Vehicles
+              </Link>
+            </li>
+            <li
+              className="list"
+              onClick={() => {
+                handleClick("starships");
+              }}>
+              <Link
+                to="/Starships"
+                className={active === "starships" ? "active" : ""}>
+                Starships
+              </Link>
+            </li>
+            <li
+              className="list"
+              onClick={() => {
+                handleClick("films");
+              }}>
+              <Link to="/Films" className={active === "films" ? "active" : ""}>
+                {" "}
+                Films
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      }
     </header>
-    
   );
 }
