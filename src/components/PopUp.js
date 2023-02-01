51ace5265480c19966508c13cf1cac424a066878
ledei <holybuzz@live.se>
2023-02-01 13:47:07 +0100

    did some more styling on pop-up

--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -11,14 +11,17 @@ export class PopUp extends React.Component {
         <div className={this.props.togglePopUp}>
           <div className="pop-up-content">
             <h2>{this.props.content.name}</h2>
-            <p>Birth year: {this.props.content.birth_year}</p>
-            <p>Gender: {this.props.content.gender}</p>
-            <p>Height: {this.props.content.height}cm</p>
-            <p>Hair color: {this.props.content.hair_color}</p>
-            <p>Eye color: {this.props.content.eye_color}</p>
 
-            <div>
-              <h2>{this.props.homeWorld.name}</h2>
+            <div className="content-detials">
+              <p>Birth year: {this.props.content.birth_year}</p>
+              <p>Gender: {this.props.content.gender}</p>
+              <p>Height: {this.props.content.height}cm</p>
+              <p>Hair color: {this.props.content.hair_color}</p>
+              <p>Eye color: {this.props.content.eye_color}</p>
+            </div>
+
+            <h2>{this.props.homeWorld.name}</h2>
+            <div className="content-home-world">
               <p>Rotation period: {this.props.homeWorld.rotation_period}h</p>
               <p>Orbital period: {this.props.homeWorld.orbital_period}days</p>
               <p>Climate: {this.props.homeWorld.climate}</p>
@@ -27,7 +30,9 @@ export class PopUp extends React.Component {
               <p>Diameter:{this.props.homeWorld.diameter}</p>
               <p>Terrain: {this.props.homeWorld.terrain}</p>
             </div>
-            <button onClick={() => this.close()}>close</button>
+            <button className="pop-up-btn" onClick={() => this.close()}>
+              close
+            </button>
           </div>
         </div>
       </>
@@ -39,12 +44,16 @@ export class PopUp extends React.Component {
       <>
         <div className={this.props.togglePopUp}>
           <div className="pop-up-content">
-            <h2>{this.props.content.title}</h2>
-            <p>Episode: {this.props.content.episode_id}</p>
-            <p>Director: {this.props.content.director}</p>
-            <p>Producer: {this.props.content.producer}</p>
-            <p>Release date: {this.props.content.release_date}</p>
-            <button onClick={() => this.close()}>close</button>
+            <div className="content-detials">
+              <h2>{this.props.content.title}</h2>
+              <p>Episode: {this.props.content.episode_id}</p>
+              <p>Director: {this.props.content.director}</p>
+              <p>Producer: {this.props.content.producer}</p>
+              <p>Release date: {this.props.content.release_date}</p>
+            </div>
+            <button className="pop-up-btn" onClick={() => this.close()}>
+              close
+            </button>
           </div>
         </div>
       </>
@@ -57,14 +66,18 @@ export class PopUp extends React.Component {
         <div className={this.props.togglePopUp}>
           <div className="pop-up-content">
             <h2>{this.props.content.name}</h2>
-            <p>Rotation period: {this.props.content.rotation_period}h</p>
-            <p>Orbital period: {this.props.content.orbital_period}days</p>
-            <p>Climate: {this.props.content.climate}</p>
-            <p>Gravity: {this.props.content.gravity}</p>
-            <p>Population: {this.props.content.population}</p>
-            <p>Diameter:{this.props.content.diameter}</p>
-            <p>Terrain: {this.props.content.terrain}</p>
-            <button onClick={() => this.close()}>close</button>
+            <div className="content-detials">
+              <p>Rotation period: {this.props.content.rotation_period}h</p>
+              <p>Orbital period: {this.props.content.orbital_period}days</p>
+              <p>Climate: {this.props.content.climate}</p>
+              <p>Gravity: {this.props.content.gravity}</p>
+              <p>Population: {this.props.content.population}</p>
+              <p>Diameter:{this.props.content.diameter}</p>
+              <p>Terrain: {this.props.content.terrain}</p>
+            </div>
+            <button className="pop-up-btn" onClick={() => this.close()}>
+              close
+            </button>
           </div>
         </div>
       </>
@@ -77,12 +90,15 @@ export class PopUp extends React.Component {
         <div className={this.props.togglePopUp}>
           <div className="pop-up-content">
             <h2>{this.props.content.name}</h2>
-            <p>classification: {this.props.content.classification}</p>
-            <p>Average height: {this.props.content.average_height} cm</p>
-            <p>Average lifespan: {this.props.content.average_lifespan}</p>
-            <p>language: {this.props.content.language}</p>
-            <div>
-              <h2>Home world:{this.props.homeWorld.name}</h2>
+            <div className="content-detials">
+              <p>classification: {this.props.content.classification}</p>
+              <p>Average height: {this.props.content.average_height} cm</p>
+              <p>Average lifespan: {this.props.content.average_lifespan}</p>
+              <p>language: {this.props.content.language}</p>
+            </div>
+
+            <h2>{this.props.homeWorld.name}</h2>
+            <div className="content-home-world">
               <p>Rotation period: {this.props.homeWorld.rotation_period}h</p>
               <p>Orbital period: {this.props.homeWorld.orbital_period}days</p>
               <p>Climate: {this.props.homeWorld.climate}</p>
@@ -91,7 +107,10 @@ export class PopUp extends React.Component {
               <p>Diameter:{this.props.homeWorld.diameter}</p>
               <p>Terrain: {this.props.homeWorld.terrain}</p>
             </div>
-            <button onClick={() => this.close()}>close</button>
+
+            <button className="pop-up-btn" onClick={() => this.close()}>
+              close
+            </button>
           </div>
         </div>
       </>
@@ -103,18 +122,22 @@ export class PopUp extends React.Component {
       <>
         <div className={this.props.togglePopUp}>
           <div className="pop-up-content">
-            <h2>{this.props.content.name}</h2>
-            <p>Model: {this.props.content.model}</p>
-            <p>Manufacturer: {this.props.content.manufacturer}</p>
-            <p>
-              Max atmosphering speed:
-              {this.props.content.max_atmosphering_speed} km/h
-            </p>
-            <p>Crew: {this.props.content.crew}</p>
-            <p>Passengers:{this.props.content.passengers}</p>
-            <p>starship class: {this.props.content.starship_class}</p>
-            <p>Cost in credits: {this.props.content.cost_in_credits}</p>
-            <button onClick={() => this.close()}>close</button>
+            <div className="content-detials">
+              <h2>{this.props.content.name}</h2>
+              <p>Model: {this.props.content.model}</p>
+              <p>Manufacturer: {this.props.content.manufacturer}</p>
+              <p>
+                Max atmosphering speed:
+                {this.props.content.max_atmosphering_speed} km/h
+              </p>
+              <p>Crew: {this.props.content.crew}</p>
+              <p>Passengers:{this.props.content.passengers}</p>
+              <p>starship class: {this.props.content.starship_class}</p>
+              <p>Cost in credits: {this.props.content.cost_in_credits}</p>
+            </div>
+            <button className="pop-up-btn" onClick={() => this.close()}>
+              close
+            </button>
           </div>
         </div>
       </>
@@ -137,7 +160,9 @@ export class PopUp extends React.Component {
             <p>Passengers:{this.props.content.passengers}</p>
             <p>starship class: {this.props.content.starship_class}</p>
             <p>Cost in credits: {this.props.content.cost_in_credits}</p>
-            <button onClick={() => this.close()}>close</button>
+            <button className="pop-up-btn" onClick={() => this.close()}>
+              close
+            </button>
           </div>
         </div>
       </>
